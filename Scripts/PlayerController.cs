d3fba0f59de694bbee683b026f2684d3c8fdc8ac
26583 <43673592+26583@users.noreply.github.com>
2019-05-08 18:24:14 +0200

    Add files via upload

---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float r = -90;
+    public int speed = 3;
+    private bool card = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W)) {
+            transform.position += transform.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            transform.position -= transform.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            r -= 1.3f;
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            r += 1.3f;
+        }
+        transform.rotation = Quaternion.Euler(0, r, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "card") {
+            Destroy(other.gameObject);
+            card = true;
+        }
+    }
+
+    public bool getCardBool()
+    {
+        return card;
+    }
+}
---
+++ Scripts/WallBehavior.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallBehavior : MonoBehaviour
+{
+    PlayerController playerScr;
+    private bool trigger = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        playerScr = GameObject.Find("player").GetComponent<PlayerController>();
+        if (trigger) {
+            transform.position -= transform.up * 2 * Time.deltaTime;
+        }
+        if(transform.position.y <= -2) {
+            trigger = false;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "player"&& playerScr.getCardBool()) {
+            trigger = true;
+        }
+    }
+}
